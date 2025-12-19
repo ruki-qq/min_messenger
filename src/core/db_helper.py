b7c3721da72d17f69bc1dc96d1f375b08929807d
rukiqq <mitaka@duck.com>
2025-12-19 16:14:37 +0300

    update DBHelper class

--- src/core/db_helper.py
+++ src/core/db_helper.py
@@ -1,9 +1,7 @@
-from asyncio import current_task
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
-    async_scoped_session,
     async_sessionmaker,
     create_async_engine,
 )
@@ -21,23 +19,14 @@ class DBHelper:
             expire_on_commit=False,
         )
 
-    def get_scoped_session(self):
-        return async_scoped_session(
-            session_factory=self.session_factory,
-            scopefunc=current_task,
-        )
-
-    async def session_dependency(self) -> AsyncGenerator[AsyncSession]:
+    async def uow_session(self) -> AsyncGenerator[AsyncSession, None]:
         async with self.session_factory() as session:
-            yield session
-            await session.close()
-
-    async def scoped_session_dependency(
-        self,
-    ) -> AsyncGenerator[async_scoped_session[AsyncSession]]:
-        session = self.get_scoped_session()
-        yield session
-        await session.remove()
+            try:
+                yield session
+                await session.commit()
+            except Exception:
+                await session.rollback()
+                raise
 
 
 db_helper = DBHelper(
